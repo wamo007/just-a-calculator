ba3106773f306abbc90ffb8e5cb60ddbc02db31b
Shamo <11028274+wamo007@users.noreply.github.com>
2024-09-24 17:39:20 +0200

    add a simple calculator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # just-a-calculator
 Simple Calculator Project for TOP
+
+All the basic function are working as intended.
---
+++ script.js
@@ -0,0 +1,208 @@
+const operators = ['+','-','*','/','%','='];
+
+//Basic Operations
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function percentage(a) {
+    return a * 0.01;
+}
+
+function minus(a) {
+    return -a ;
+}
+
+//Setting all variables to 0 before using them
+let inputNum = '';
+let secondNum = '';
+let inputSign = '';
+let result = '';
+
+const displayInput = document.querySelector('.input'); //imports display's input side
+const displayOutput = document.querySelector('.output'); //imports display's answer side
+const operationsArray = document.querySelectorAll('.operations'); //imports all signs (except pound sign)
+const numberArray = document.querySelectorAll('.number'); //imports numpad
+
+displayOutput.textContent = 0;
+
+//Get first number of the equation
+const getFirstNum = function() {
+    numberArray.forEach(function(number) {
+        number.addEventListener('click', function() {
+            if (!inputSign) {
+                inputNum += `${number.textContent}`;
+                if (inputNum.length > 11) {
+                    inputNum = inputNum.substring(0, 11);
+                }
+                displayInput.textContent = parseFloat(inputNum);
+            }
+        });
+    });
+};
+
+//Get a sign of equation
+const getSign = function() {
+    operationsArray.forEach(function(operation) {
+        operation.addEventListener('click', function() {
+            if (inputNum && !secondNum) {
+                inputSign = operation.textContent;
+            } else if (inputNum && secondNum) {
+                operate();
+                inputSign = operation.textContent;
+            }
+        });
+    });
+}
+
+//Get second number of the equation
+const getSecondNum = function() {
+    numberArray.forEach(function(number) {
+        number.addEventListener('click', function() {
+            if (inputSign) {
+                secondNum += `${number.textContent}`;
+                if (secondNum.length > 11) {
+                    secondNum = secondNum.substring(0, 11);
+                }
+                displayInput.textContent = parseFloat(secondNum);
+            }
+        })
+    });
+}
+
+//Solves the equation
+function operate() {
+    if (!secondNum) {
+        secondNum = inputNum;
+    };
+    switch (inputSign) {
+        case '+':
+            result = addition(parseFloat(inputNum), parseFloat(secondNum));
+            break;
+        case '-':
+            result = subtraction(parseFloat(inputNum), parseFloat(secondNum));
+            break;
+        case '*':
+            result = multiplication(parseFloat(inputNum), parseFloat(secondNum));
+            break;
+        case '/':
+            if (parseFloat(secondNum) === 0) {
+                result = 'lol what?';
+                break;
+            } else {
+            result = division(parseFloat(inputNum), parseFloat(secondNum));
+            break;
+            }
+        default:
+            result = 'lol what?';
+            break;
+    }
+    if (result.toString().length > 10) {
+        result = result.toString().substring(0, 10);
+    }
+    displayOutput.textContent = result;
+
+    inputNum = result;
+    secondNum = '';
+    inputSign = '';
+};
+
+//Listener for AC button
+document.querySelector('.clean').addEventListener('click', function() {
+    displayInput.textContent = '';
+    displayOutput.textContent = 0;
+    inputNum = '';
+    secondNum = '';
+    inputSign = '';
+    result = '';
+});
+
+//Listener for the Equal (=) sign
+document.querySelector('.equals').addEventListener('click', () =>
+    ((inputNum) && (secondNum) && (inputSign)) ? operate : displayOutput.textContent);
+
+//Listener for the Per cent (%) sign
+document.querySelector('.percent').addEventListener('click', function() {
+    if ((displayOutput.textContent = '0') && (!secondNum)) {
+        inputNum = parseFloat(inputNum) * 0.01;
+        displayInput.textContent = inputNum.toString().substring(0,10);
+        displayOutput.textContent = inputNum.toString().substring(0,10);
+    } else if ((displayOutput.textContent = '0') && (secondNum)) {
+        operate();
+        inputNum = parseFloat(inputNum) * 0.01;
+        displayInput.textContent = inputNum.toString().substring(0,10);
+        displayOutput.textContent = inputNum.toString().substring(0,10);
+    } else {
+        result = parseFloat(result) * 0.01;
+        inputNum = result;
+        displayInput.textContent = inputNum.toString().substring(0,10);
+        displayOutput.textContent = result.toString().substring(0,10);
+    }
+});
+
+//Listener for Plus-Minus (+/-) sign
+document.querySelector('.plus-minus').addEventListener('click', function() {
+    if (!secondNum) {
+        inputNum = 0 - parseFloat(inputNum);
+        inputNum = inputNum.toString();
+        displayInput.textContent = inputNum.toString().substring(0,10);
+    } else if (secondNum) {
+        // operate();
+        secondNum = 0 - parseFloat(secondNum);
+        secondNum = secondNum.toString();
+        displayInput.textContent = secondNum.toString().substring(0,10);
+    } else {
+        result = 0 - parseFloat(result);
+        inputNum = result.toString();
+        displayInput.textContent = inputNum.toString().substring(0,10);
+    }
+});
+
+//Listener for the Pound (.) button
+document.querySelector('.pound').addEventListener('click', function() {
+    if ((!secondNum) && (!inputSign) && (!result)) {
+        if (!inputNum.toString().includes('.')) {
+            inputNum = inputNum.toString() + '.';
+            displayInput.textContent = inputNum.toString().substring(0,10);
+        }
+    } else if ((!secondNum) && (inputSign) && (inputNum) && (!result)) {
+        if (!secondNum.toString().includes('.')) {
+            secondNum = '0.';
+            displayInput.textContent = secondNum.toString().substring(0,10);
+        }
+    } else if ((!secondNum) && (inputSign) && (inputNum)) {
+        if (!secondNum.toString().includes('.')) {
+            secondNum = '0.';
+            displayInput.textContent = secondNum.toString().substring(0,10);
+        }
+    } else if ((inputSign) && (secondNum)) {
+        if (!secondNum.toString().includes('.')) {
+            secondNum = secondNum.toString() + '.';
+            displayInput.textContent = secondNum.toString().substring(0,10);
+        }
+    } else {
+        if (!inputNum.toString().includes('.')) {
+            result = result.toString() + '.';
+            inputNum = result;
+            displayInput.textContent = inputNum.toString().substring(0,10);
+            displayOutput.textContent = result.toString().substring(0,10);
+        }
+    }
+})
+
+//Functions are called in the order below
+getFirstNum();
+getSign();
+getSecondNum();
